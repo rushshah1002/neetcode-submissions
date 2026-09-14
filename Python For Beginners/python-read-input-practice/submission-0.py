7db93a7578d826c3cc67d6b8d1cb887bed140b0f
rushshah1002 <rush.shah.1002@gmail.com>
2026-09-14 19:40:33 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,14 @@
+def add_two_numbers() -> int:
+    l = input().split(",")
+    i=0
+    for ele in l:
+        i+= int(ele)
+    return i
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
