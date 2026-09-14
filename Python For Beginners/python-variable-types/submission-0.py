fa0fc13723b35f4256bd1dd446318a32cd894492
rushshah1002 <rush.shah.1002@gmail.com>
2026-09-14 13:40:41 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 5
+float_type = 5.1
+bool_type = False
+string_type = 'hi'
+list_type = [1,2]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
